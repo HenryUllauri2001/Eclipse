d3c6b4ea647b487307eb0a5a76c41dae031ae8b0
HenryUllauri <henrypaulullauricarchi@gmail.com>
2022-07-10 19:46:33 -0500

    Primer commit

---
+++ src/main/java/com/example/domain/Provincia.java
@@ -0,0 +1,35 @@
+package com.example.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Provincia {
+	@Id
+	@GeneratedValue
+	@Column(nullable = false)
+	private Long id;
+	@Column (nullable = false)
+	private String nombre;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
